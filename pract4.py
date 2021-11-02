e078849b672d07addb7070ffda85ecb899849e5d
SpecTrumd3v <17041298@itdurango.edu.mx>
2021-11-02 17:04:59 -0600

    primos

--- pract4.py
+++ pract4.py
@@ -1,10 +1,8 @@
-
-
 for m in range (0,100):
     if m>1:
         for n in  range(2,m):
             if (m%n) == 0:
                 break
-    else:    
-        print (m)
+        else:    
+                print (m)
                          
