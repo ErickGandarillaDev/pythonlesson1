d58ab71aadca0cb079e4a3439258c275a051b8b2
SpecTrumd3v <17041298@itdurango.edu.mx>
2021-11-02 15:35:13 -0600

    segunda

--- pract1.py
+++ pract1.py
@@ -1,3 +1,25 @@
 var = 'mundo'
+num = 12132
+num2= 5789
+
+flotante = 123.41f
+flotante2 = 456.12f
+
+carac = 'c'
+carac2= 'd'
+
+bol = True
+bol2 = False
+bol3 = None
 
 print (var)
+print (carac+carac2)
+print (var + carac )
+
+print (num+num2)
+print (flotante+flotante2)
+
+print (not bol )
+print (not bol2 )
+print (bol or bol2)
+print (bol and bol2)
